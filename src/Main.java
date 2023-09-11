3d8d884644c8a1fb63f0141102100407b50ee9d3
atrenes <hanalainendm@gmail.com>
2023-09-11 21:11:51 +0300

    argument workarounds

--- src/Main.java
+++ src/Main.java
@@ -3,30 +3,39 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    final static String PREFIX = "processed_";
 
-        if (args.length < 1) {
+    public static void main(String[] args) {
+        if (args.length < 2) {
             System.out.println("""
-                    Too little arguments!
+                    Wrong number of arguments
+                    
                     arg 1: file name
                     arg 2: int key
-                    arg 3: locale (ru/eng)
                     """);
             System.exit(1);
         }
 
-        try (FileInputStream fileInputStream = new FileInputStream(args[0]);
-             FileOutputStream fileOutputStream = new FileOutputStream((args[0]) + "_encrypted")) {
+        int key = 0;
+        try {
+            key = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage());
+        }
 
-                byte[] buffer = new byte[16];
+        try (FileInputStream fileInputStream = new FileInputStream(args[0]);
+             FileOutputStream fileOutputStream = new FileOutputStream(PREFIX + (args[0]))) {
+
+                byte[] buffer = new byte[1024];
                 int count;
 
                 while ( (count = fileInputStream.read(buffer)) != -1 ) {
-                    fileOutputStream.write(Caesar.encode(buffer), 0, count);
+                    fileOutputStream.write(Caesar.encodeBytes(buffer, key), 0, count);
                 }
-
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+
+        System.out.println(args[0] + " encrypted as " + PREFIX + args[0]);
     }
 }
